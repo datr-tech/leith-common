0e00f214bd71409db3fb34498e965b0052184423
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-18 14:44:12 +0100

    fix: update cspell

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as commonLoggerModule from '@datr.tech/leith-common-logger';
+import * as commonServicesModule from '@datr.tech/leith-common-services';
+
+export const commonLogger = commonLoggerModule;
+export const commonServices = commonServicesModule;
